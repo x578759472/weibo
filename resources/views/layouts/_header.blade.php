a07b0a51612672a03e01ba794293b650576cb124
许力 <578759472@qq.com>
2019-01-21 22:30:08 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a href="/" class="navbar-brand">Weibo App</a>
+    <ul class="navbar-nav justify-content-end">
+      <li class="nav-litem"><a href="/help" class="nav-link">帮助</a></li>
+      <li class="nav-litem"><a href="#" class="nav-link">登陆</a></li>
+    </ul>
+  </div>
+</nav>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,19 +8,12 @@
   <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a href="/" class="navbar-brand">Weibo App</a>
-      <ul class="navbar-nav justify-content-end">
-        <li class="nav-litem"><a href="/help" class="nav-link">帮助</a></li>
-        <li class="nav-litem"><a href="#" class="nav-link">登陆</a></li>
-      </ul>
-    </div>
-  </nav>
+@include('layouts._header')
 
 
   <div class="container">
       @yield('content')
+      @include('layouts._footer')
   </div>
 
 </body>
