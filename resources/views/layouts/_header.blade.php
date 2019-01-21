51c403f38dab19832ee9477d3db8fd20b02bccaa
许力 <578759472@qq.com>
2019-01-21 22:43:48 +0800

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a href="/" class="navbar-brand">Weibo App</a>
+    <a href="{{route('home')}}" class="navbar-brand">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-litem"><a href="/help" class="nav-link">帮助</a></li>
+      <li class="nav-litem"><a href="{{route('help')}}" class="nav-link">帮助</a></li>
       <li class="nav-litem"><a href="#" class="nav-link">登陆</a></li>
     </ul>
   </div>
