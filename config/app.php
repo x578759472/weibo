bc8dc1a188b32e6c5544351405c31ef9a48bb28c
许力 <578759472@qq.com>
2019-01-22 14:43:43 +0800

    1

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'weibo'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://weibo.test'),
 
     'asset_url' => env('ASSET_URL', null),
 
