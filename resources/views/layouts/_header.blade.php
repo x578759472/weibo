2bac3fc37d55faa2b73e1a2bfcf0d2a424148f4f
许力 <578759472@qq.com>
2019-01-22 17:18:58 +0800

    用户显示页面

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
     <a href="{{route('home')}}" class="navbar-brand">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
       <li class="nav-litem"><a href="{{route('help')}}" class="nav-link">帮助</a></li>
-    <li class="nav-litem"><a href="{{route('signup')}}" class="nav-link">登陆</a></li>
+      <li class="nav-litem"><a href="{{route('signup')}}" class="nav-link">登陆</a></li>
     </ul>
   </div>
 </nav>
