e99b7a3f779187c6e5bb4cd92930392714f27b36
许力 <578759472@qq.com>
2019-01-24 14:14:45 +0800

    代码提示ide

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Auth;
+use Illuminate\Support\Facades\DB;
 use Mail;
 class UsersController extends Controller
 {
