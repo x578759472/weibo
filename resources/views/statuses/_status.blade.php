6b81cc787cb02d314cac339aa1488f6a83f49221
许力 <578759472@qq.com>
2019-01-24 16:19:33 +0800

    微博数据模型

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -6,4 +6,13 @@
         <h5 class="mt-0 mb-1">{{$user->name}} <small>  {{$status->created_at->diffForHumans()}}</small>  </h5>
         {{$status->content}}
     </div>
+    @can('destroy' ,$status)
+        <form action="{{route('statuses.destroy',$status->id)}}" method="post">
+            {{csrf_field()}}
+            {{method_field('DELETE')}}
+            <button type="submit" class="btn btn-danger btn-sm">删除</button>
+        </form>
+
+        @endcan
+
 </li>
