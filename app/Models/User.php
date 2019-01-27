b454996a5e409e02e91decdb7cbac8e5c9b1aeb4
许力 <578759472@qq.com>
2019-01-27 05:59:04 +0000

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,6 +64,41 @@ class User extends Authenticatable
          return $this->hasMany(Status::class);
     }
 
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+    }
+
+
+    public function following()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->sync($user_ids,false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if(!is_array($user_ids)){
+            $user_ids = compact('user_ids');
+        }
+
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
     public static function boot()
     {
         parent::boot();
